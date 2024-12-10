5b33ac7f23a2f73cd78d2ec0a7e17742faa8d57d
9oelM <hj923@hotmail.com>
2024-12-10 12:38:28 +0900

    test

--- tests/Bug.spec.ts
+++ tests/Bug.spec.ts
@@ -66,10 +66,79 @@ describe('Bug', () => {
         // blockchain and contractA are ready to use
     });
 
-    it('time test', async () => {
+    it('time test (not fixed)', async () => {
         const increaser = await blockchain.treasury('increaser');
         blockchain.now = Math.floor(Date.now() / 1000);
-        console.log(blockchain.lt)
+
+        const messages = await blockchain.sendMessageIter(
+            internal({
+                from: increaser.address,
+                to: contractA.address,
+                value: toNano(`0.5`),
+                body: beginCell()
+                    .storeUint(Opcodes.increase, 32)
+                    .storeUint(0, 64)
+                    .storeUint(1, 32)
+                .endCell(),
+            }),
+            {
+                now: blockchain.now,
+            }
+        );
+
+        console.log({
+            step: 0,
+            from_contract_a: await contractA.getCounterAndTimestamp(),
+            from_contract_b: await contractB.getCounterAndTimestamp(),
+            blockchain_now: blockchain.now,
+        })
+
+        blockchain.now += 1000;
+
+        console.log({
+            step: 1,
+            from_contract_a: await contractA.getCounterAndTimestamp(),
+            from_contract_b: await contractB.getCounterAndTimestamp(),
+            blockchain_now: blockchain.now,
+        })
+
+        await executeTill(messages, {
+            from: increaser.address,
+            to: contractA.address,
+            success: true,
+        });
+
+        blockchain.now += 1000;
+
+        console.log({
+            step: 2,
+            from_contract_a: await contractA.getCounterAndTimestamp(),
+            from_contract_b: await contractB.getCounterAndTimestamp(),
+            blockchain_now: blockchain.now,
+        })
+
+        const end = await executeTill(messages, {
+            from: contractA.address,
+            to: contractB.address,
+            success: true,
+        });
+
+        console.log({
+            step: 3,
+            from_contract_a: await contractA.getCounterAndTimestamp(),
+            from_contract_b: await contractB.getCounterAndTimestamp(),
+            blockchain_now: blockchain.now,
+        })
+
+
+        expect(end).toHaveLength(1)
+        expect(end[0].outMessagesCount).toBe(0)
+        expect((await messages.next()).done).toBe(true);
+    });
+    
+    it('time test (fixed)', async () => {
+        const increaser = await blockchain.treasury('increaser');
+        blockchain.now = Math.floor(Date.now() / 1000);
 
         const messages = await blockchain.sendMessageIter(
             internal({
